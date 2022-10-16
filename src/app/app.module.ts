3e84065a6f9a06402812b5afd407d5ae55c54753
Elda-Dani <eldadani2001@gmail.com>
2022-10-16 21:22:03 +0530

    addd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddDemoComponent } from './add-demo/add-demo.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AddDemoComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
